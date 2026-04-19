e5de74a20dc90ffb51e5794e0b6b682d314d1714
naokichi-nyannz <250805457+naokichi-nyannz@users.noreply.github.com>
2026-04-19 14:56:24 +0900

    サイト名を「なおきちのお天気ブログ」に変更

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'なおきちのお天気ブログ';
+export const SITE_DESCRIPTION = '気象予報士試験の解説や気象に関する情報を発信しています。';
